9e67f3fa75b297ee7921965781e7b382762e87fb
chaitunice <52159476+chaitunice@users.noreply.github.com>
2023-11-17 00:56:28 +0530

    addded .js at end

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import './App.scss';
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import MyButton from './components/myButton';
+import MyButton from './components/myButton.js';
 // import './App.css';
 import './App1.css';
 import './App2.scss';
